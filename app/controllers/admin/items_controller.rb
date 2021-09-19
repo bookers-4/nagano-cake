235a3bad96dab12da07c31db4442f82eb59d89af
yusei1125 <sk11250925@gmail.com>
2021-09-19 10:57:31 +0000

    adminitem

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,42 @@
+class Admin::ItemsController < ApplicationController
+    def new
+    @item = Item.new
+    @genres = Genre.all
+    end
+    
+    def create
+        @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    end  
+    end
+    
+    def index
+    @items = Item.page(params[:page]).per(10)    
+    end
+    
+    def show
+    @item = Item.find(params[:id])    
+    end
+    
+    def edit
+    @item = Item.find(params[:id])    
+    end
+    
+    def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      redirect_to request.referer,notice:"変更を保存できませんでした"
+    end
+    end
+  
+  
+  
+  private
+    def item_params
+      params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
+    end
+
+end
