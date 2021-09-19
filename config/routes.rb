52dc1b16942e06e002123862c29cd975cf82c780
yusei1125 <sk11250925@gmail.com>
2021-09-19 07:14:12 +0000

    管理者ログイン実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
 }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   #管理者ルーティング
-  namespace :admins do
+  namespace :admin do
     root to: "homes#top"
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
