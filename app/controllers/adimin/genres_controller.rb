95358d023a02a72177af11538ce4cc9f129ded4d
Haruibos <sunada.381724@gmail.com>
2021-09-19 08:17:11 +0000

    Add genres index-page

---
+++ app/controllers/adimin/genres_controller.rb
@@ -0,0 +1,32 @@
+class Adimin::GenresController < ApplicationController
+
+ def index
+    @genres = Genre.all
+    @genre = Genre.new
+ end
+
+ def create
+     genre = Genre.new(genre_params)
+     genre.save
+     redirect_to admin_genres_pash
+ end
+
+ def edit
+  @genre = Genre.all
+ end
+
+ def update
+  @genre = Genre.find()
+ end
+
+ private
+  # ストロングパラメータ
+  def genre_params
+    params.require(:genre).permit(:title, :body)
+  end
+  
+# paramsはRailsで送られてきた値を受け取るためのメソッドです。
+# requireでデータのオブジェクト名(ここでは:list)を指定し、permitでキー（:title,:body）を指定しています。
+# これにより、list_paramsの中にフォームで入力されたデータが格納されます。
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ Rails.application.routes.draw do
 }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   #管理者ルーティング
-  namespace :admins do
+  namespace :admin do
     root to: "homes#top"
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [ :show, :update] 
-    resources :order_details, only: [:update] 
+    resources :orders, only: [ :show, :update]
+    resources :order_details, only: [:update]
   end
 end
