d6f9ee166fb9969e806b783e0f6476be486e6e2e
yusei1125 <sk11250925@gmail.com>
2021-09-19 05:22:02 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   devise_for :customers
-  devise_for :admins
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
+  
+  #管理者ルーティング
+  namespace :admins do
+    root to: "homes#top"
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [ :show, :update] 
+    resources :order_details, only: [:update] 
+  end
 end
