eca3ab4d4102439a5936e1de92dd167a5d283920
Haruibos <sunada.381724@gmail.com>
2021-09-19 08:50:26 +0000

    Add genres edit-page

--- db/migrate/20210918094805_create_addresses.rb
+++ db/migrate/20210918094805_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[5.0]
   def change
     create_table :addresses do |t|
-      t.intenger :customer_id, foreign_key: ture , null: false
+      t.integer :customer_id, foreign_key: ture , null: false
       t.string :postl_code, null: false
       t.string :address, null: false
       t.string :name, null: false
