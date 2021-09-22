e995b4258bb971e689330fd1445ff4fb4e6475e7
keitakitamoto <keita0410.k.k@gmail.com>
2021-09-22 08:26:24 +0000

    退会機能とマイページ

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,9 @@ Rails.application.routes.draw do
    
     resources :items,only: [:index,:show]
 
-
+    get "customers/unsubscribe" => "customers#unsubscribe"
+    patch "customers/withdraw" => "customers#withdraw"  
+    #仮追加
     
 
     resources :addresses,only: [:index,:create,:edit,:update,:destroy]
